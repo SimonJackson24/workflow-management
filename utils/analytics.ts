f7b1f5d3fca7c160d87155b59f4bcc203fe2a7a0
SimonJackson24 <simoncallaghan21@gmail.com>
2024-12-08 09:39:55 +0000

    Create analytics.ts

---
+++ utils/analytics.ts
@@ -0,0 +1,8 @@
+// utils/analytics.ts
+
+export const initializeAnalytics = () => {
+  if (import.meta.env.PROD) {
+    // Initialize analytics services
+    // Example: Google Analytics, Mixpanel, etc.
+  }
+};
