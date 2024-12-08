bc01978904d1f8267068992d490b00b4de5dbeda
SimonJackson24 <simoncallaghan21@gmail.com>
2024-12-08 10:37:38 +0000

    Create StoreProvider.tsx

---
+++ src/store/StoreProvider.tsx
@@ -0,0 +1,21 @@
+// src/store/StoreProvider.tsx
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './configureStore';
+import { LoadingScreen } from '@/components/common/LoadingScreen';
+
+interface StoreProviderProps {
+  children: React.ReactNode;
+}
+
+export const StoreProvider: React.FC<StoreProviderProps> = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<LoadingScreen />} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
+};
