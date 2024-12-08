6172f0221aceea006b547af9e0919600928f36f2
SimonJackson24 <simoncallaghan21@gmail.com>
2024-12-08 11:28:00 +0000

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,24 @@
 // src/utils/api.ts
 
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // Handle unauthorized
+    }
+    return Promise.reject(error);
+  }
+); 
+
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { analytics } from './analytics';
 
