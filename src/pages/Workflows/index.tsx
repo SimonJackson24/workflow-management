e55af19e11410b1d123d4a5c3989c6715303d946
SimonJackson24 <simoncallaghan21@gmail.com>
2024-12-08 11:12:49 +0000

    Create index.tsx

---
+++ src/pages/Workflows/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Workflows: React.FC = () => {
+  return (
+    <div className="workflows">
+      <h1>Workflows</h1>
+    </div>
+  );
+};
+
+export default Workflows; 
