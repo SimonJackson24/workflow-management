611b6a6061d097093a08f89f661a3710d3423c8a
SimonJackson24 <simoncallaghan21@gmail.com>
2024-12-08 09:39:04 +0000

    Create monitoring.ts

---
+++ utils/monitoring.ts
@@ -0,0 +1,15 @@
+// utils/monitoring.ts
+
+import { ReportHandler } from 'web-vitals';
+
+export const monitorWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
