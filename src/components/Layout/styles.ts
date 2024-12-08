8212f260fc3cf13c4c62442fb8d5e290b5d59df7
SimonJackson24 <simoncallaghan21@gmail.com>
2024-12-08 11:25:04 +0000

    Create styles.ts

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material/styles';
+
+export const LayoutContainer = styled('div')`
+  display: flex;
+  min-height: 100vh;
+`;
+
+export const MainContent = styled('main')`
+  flex-grow: 1;
+  padding: 24px;
+  margin-left: 240px;
+  margin-top: 64px;
+`; 
